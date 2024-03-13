5181469812db9f8ee61f3c9be0122c662944d0f7
angelakai <95471906+angelakai@users.noreply.github.com>
2024-03-13 16:56:45 +0200

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,11 @@
+a = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+
+#Initialize an empty string to store the converted characters
+flag = ""
+
+#Iterate through each element in the list
+for i in range(len(a)):
+    #Convert ASCII value to character and append to the 'flag' string
+    flag = flag + chr(a[i])
+
+print(flag)
