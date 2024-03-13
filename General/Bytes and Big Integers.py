0fca1296479976362ced64eddee77182de6f35e2
angelakai <95471906+angelakai@users.noreply.github.com>
2024-03-13 17:04:32 +0200

    Bytes and Big Integers.py

---
+++ General/Bytes and Big Integers.py
@@ -0,0 +1,8 @@
+from Crypto.Util.number import *
+
+a=11515195063862318899931685488813747395775516287289682636499965282714637259206269
+
+#Converting long variable to bytes
+bytes=long_to_bytes(a)
+
+print(bytes)
